25852fc9df48d02e10d061a4bd6cd54aabd68f38
ka ho kwok <caesarkaho@gmail.com>
2023-06-30 11:22:47 +0800

    canvas tool done

--- canvas.js
+++ canvas.js
@@ -2,10 +2,46 @@ const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 const clearBtn = document.querySelector("#clear");
 const cardEditArea = document.querySelector(".card-edit-area");
+const colorPicker = document.querySelector("#changeColor");
+const saveBtn = document.querySelector(".save-canvas");
+const cleanBtn = document.querySelector("#uploadCancel");
+const penBtn = document.querySelector("#penBtn");
+const markerBtn = document.querySelector("#markerBtn");
+const rubberBtn = document.querySelector("#rubberBtn");
+
+// 監聽顏色選擇器的變化
+colorPicker.addEventListener("change", function() {
+  // 取得選擇的顏色
+  const color = colorPicker.value;
+  
+  // 設置畫筆的顏色為選擇的顏色
+  ctx.strokeStyle = color;
+});
 
 // 設定線條的相關數值
 ctx.lineWidth = 4;
 ctx.lineCap = "round";
+
+// 點擊簽字筆按鈕
+penBtn.addEventListener("click", function() {
+  ctx.globalCompositeOperation = "source-over"; // 將繪圖模式設置為預設模式
+  ctx.lineWidth = 4;
+  ctx.lineCap = "round";
+});
+
+// 點擊麥克筆按鈕
+markerBtn.addEventListener("click", function() {
+  ctx.globalCompositeOperation = "source-over"; // 將繪圖模式設置為預設模式
+  ctx.lineWidth = 10;
+  ctx.lineCap = "round";
+});
+
+// 點擊橡皮擦按鈕
+rubberBtn.addEventListener("click", function() {
+  ctx.globalCompositeOperation = "destination-out"; // 將繪圖模式設置為目標擦除模式
+  ctx.lineWidth = 20;
+  ctx.lineCap = "round";
+});
 
 // 設置狀態來確認滑鼠 / 手指是否按下或在畫布範圍中
 let isPainting = false;
@@ -72,12 +108,24 @@ canvas.addEventListener("touchmove", draw);
 
 clearBtn.addEventListener("click", reset);
 
-const showImage = document.querySelector(".show-img");
+
+
+
 
 function saveImage() {
-  // 圖片儲存的類型選擇 png ，並將值放入 img 的 src
   const newImg = canvas.toDataURL("image/png");
-  showImage.src = newImg;
+  localStorage.setItem('img', newImg);
 }
 
 saveBtn.addEventListener("click", saveImage);
+
+
+
+function cleanImage() {
+  // 清除 Local Storage 中的圖像資料
+  localStorage.removeItem('img');
+  // 清除 canvas 中的內容
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+cleanBtn.addEventListener("click", cleanImage);
